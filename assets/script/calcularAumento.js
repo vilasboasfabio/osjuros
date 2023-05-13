7d56e591179a9d2fa84acaf646e7e9639ad4b7fb
Fábio Vilas Boas <vilasboasfabio564@gmail.com>
2023-05-13 12:36:25 -0300

    ajustes

--- assets/script/calcularAumento.js
+++ assets/script/calcularAumento.js
@@ -11,39 +11,24 @@ function calcularAumento() {
         aumento = sal * 0.08;
         final = sal + aumento;
         porcentagem = 8;
-        document.getElementById("sa").innerHTML = `Seu salário atual é: ${sal}`;
-        document.getElementById("por").innerHTML = `Seu aumento será de: ${porcentagem}%`;
-        document.getElementById("amt").innerHTML = `O valor do seu aumento em dinheiro será de: R$${aumento}`;
-        document.getElementById("final").innerHTML = `Seu salário final será de: R$${final}`;
-
     }else if (sal >= 6001 && sal <= 10000) {
         aumento = sal * 0.06;
         final = sal + aumento;
         porcentagem = 6;
-        document.getElementById("sa").innerHTML = `Seu salário atual é: ${sal}`;
-        document.getElementById("por").innerHTML = `Seu aumento será de: ${porcentagem}%`;
-        document.getElementById("amt").innerHTML = `O valor do seu aumento em dinheiro será de: R$${aumento}`;
-        document.getElementById("final").innerHTML = `Seu salário final será de: R$${final}`;
-
     }else if (sal >= 10001 && sal <= 15000) {
         aumento = sal * 0.04;
         final = sal + aumento;
         porcentagem = 4;
-        document.getElementById("sa").innerHTML = `Seu salário atual é: ${sal}`;
-        document.getElementById("por").innerHTML = `Seu aumento será de: ${porcentagem}%`;
-        document.getElementById("amt").innerHTML = `O valor do seu aumento em dinheiro será de: R$${aumento}`;
-        document.getElementById("final").innerHTML = `Seu salário final será de: R$${final}`;
-
     }else if(sal > 15000 && sal < 40000){
         aumento = sal * 0.03;
         final = sal + aumento;
         porcentagem = 3;
-        document.getElementById("sa").innerHTML = `Seu salário atual é: ${sal}`;
-        document.getElementById("por").innerHTML = `Seu aumento será de: ${porcentagem}%`;
-        document.getElementById("amt").innerHTML = `O valor do seu aumento em dinheiro será de: R$${aumento}`;
-        document.getElementById("final").innerHTML = `Seu salário final será de: R$${final}`;
-
     }else{
         alert("O que você quer de mim criatura?")
     }
+    document.getElementById("sa").innerHTML = `Seu salário atual é: ${sal}`;
+    document.getElementById("por").innerHTML = `Seu aumento será de: ${porcentagem}%`;
+    document.getElementById("amt").innerHTML = `O valor do seu aumento em dinheiro será de: R$${aumento}`;
+    document.getElementById("final").innerHTML = `Seu salário final será de: R$${final}`;
+
 }
