a721a40dff34ee7746033febc45a071ab5dc41c5
vilasboasfabio <vilasboasfabio564@gmail.com>
2023-06-11 21:15:53 -0300

    Mais um

--- assets/script/notas.js
+++ assets/script/notas.js
@@ -1,41 +1,50 @@
-let iniciar = prompt("Olá, deseja adicionar algum aluno?")
-let nome = prompt("Insira o nome do aluno")
-let nota = ("Insira a nota do aluno")
-let soma;
-let média;
-let maior;
-let menor;
-let contador = 0;
-let alunos = [];
-let notas = [];
-let continuar = true;
+// let iniciar;
+// let nome;
+// let nota;
+// let soma = 0;
+// let média = 0;
+// let contador = 0;
+// let alunos = [];
+// let notas = [];
+// let continuar = true;
+// let maior = 0;
+// let menor = 0;
+// let maiorAluno;
+// let menorAluno;
 
-while(continuar){
-    alunos.push(nome)
-    notas.push(nota)
-    soma += nota;
-    contador++;
-    if(contador == 1){
-        maior = nota;
-        menor = nota;
-    }else{
-        if(nota > maior){
-            maior = nota;
-        }
-        if(nota < menor){
-            menor = nota;
-        }
-    }
-    média = soma / contador;
-    continuar = confirm("Deseja continuar?")
-    if(continuar){
-        nome = prompt("Insira o nome do aluno")
-        nota = prompt("Insira a nota do aluno")
-    }
-}
-console.log("A maior nota é: " + maior)
-console.log("A menor nota é: " + menor)
-console.log("A média da turma é: " + média)
-console.log("Os alunos são: " + alunos)
-console.log("As notas são: " + notas)
 
+
+// while(continuar){
+//     nome = prompt("Insira o nome do aluno")
+//     nota = prompt("Insira a nota do aluno")
+//     iniciar = prompt("Olá, deseja adicionar algum aluno?")
+//     if(iniciar == "não"){
+//         continuar = false
+//     }else{
+//         continuar = true
+//     }
+//     alunos.push(nome)
+//     notas.push(nota)
+//     soma += parseInt(nota)
+//     contador++
+//     if(nota > maior){
+//         maior = nota
+//         maiorAluno = nome
+//     } else if(nota < menor){
+//         menor = nota
+//         menorAluno = nome
+//     }
+
+//     média = soma / contador;
+   
+    
+
+// }
+// console.log("A maior nota é: " + maior)
+// console.log("A menor nota é: " + menor)
+// console.log("A média da turma é: " + média)
+// console.log("Os alunos são: " + alunos)
+// console.log("As notas são: " + notas)
+// console.log("O total de alunos é: " + contador)
+// console.log("O aluno com a maior nota é: " + maiorAluno)
+// console.log("O aluno com a menor nota é: " + menorAluno)
